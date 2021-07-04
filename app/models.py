0bc33eaea8fb089f4ebf9548664c082fcc58d7f0
森祐一朗 <apotikisin4632@gmail.com>
2021-07-04 20:47:14 +0900

    first commit

--- app/models.py
+++ app/models.py
@@ -1,14 +1,20 @@
+from enum import unique
 from sqlalchemy.orm import backref
-from app import db
+from flask_login import UserMixin
 
-# db.create_all()
+from app import db, login_manager
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    name = db.Column(db.String(length=20), nullable=False)
-    password = db.Column(db.String(length=20), nullable=False)
-    posts = db.relationship('Post', backref='user', lazy=True)
+    name = db.Column(db.String(length=20), nullable=False, unique=True)
+    password = db.Column(db.String(length=20), nullable=False, unique=True)
+    # posts = db.relationship('Post', backref='user', lazy=True)
 
     db.create_all()
 
@@ -22,22 +28,4 @@ class User(db.Model):
     def save(user_data):
         db.create_all()
         db.session.add(user_data)
-        db.session.commit()
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    content = db.Column(db.String(length=200), nullable=False)
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
-
-    # def __init__(self, content, user_id):
-    #     self.content = content
-    #     self.user_id = user_id
-
-    def __repr__(self) -> str:
-        return f'Post {self.content}'
-
-    def post(content):
-        db.create_all()
-        db.session.add(content)
         db.session.commit()
--- webserver.py
+++ webserver.py
@@ -1,9 +1,9 @@
-from flask import render_template, redirect, request, session, url_for
+from flask import flash, render_template, redirect, request, url_for
+from flask_login import login_user, logout_user, login_required, current_user
 
 from app import app
 
-# import models
-from app.models import User, Post
+from app.models import User
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -13,92 +13,71 @@ def index():
         app.logger.info('初期表示')
         return render_template('index.html')
     elif request.method == "POST":
-        app.logger.info('ユーザー登録')
+        try:
+            app.logger.info('ユーザー登録')
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            if User.query.filter_by(name=username, password=password).first() is not None:
+                flash('その名前もしくはパスワードはすでに登録されています。登録し直してください。')
+                return render_template('index.html')
+
+            user_data = User(username, password)
+            User.save(user_data)
+            app.logger.info('ユーザー登録完了')
+
+            # Postテーブルに追加するためのユーザーID
+            user_id = User.query.filter_by(name=username, password=password).first().id
+            login_user(user_data)
+
+            app.logger.info('ユーザーID:' + str(user_id))
+            return redirect(url_for('login'))
+        except:
+            return redirect(url_for('index'))
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        app.logger.info('ログインページ')
+        return render_template('login.html')
+    elif request.method == "POST":
+        app.logger.info('ログイン認証開始')
+        
         username = request.form.get('username')
         password = request.form.get('password')
+        user = User.query.filter_by(name=username, password=password).first()
 
-        user_data = User(username, password)
-        User.save(user_data)
-        app.logger.info('ユーザー登録完了')
+        if user is not None:
+            login_user(user)
+            app.logger.info('ログイン成功')
+            flash('ログインに成功しました')
 
-        # Postテーブルに追加するためのユーザーID
-        user_id = User.query.filter_by(name=username, password=password).first().id
-        # セッションに保存
-        session['user_id'] = user_id
-        session['name'] = username
-        session['password'] = password
+            return redirect(url_for('toppage'))
+        else:
+            app.logger.info('ログインに失敗しました')
 
-        app.logger.info('ユーザーID:' + str(user_id))
-        return redirect(url_for('top'))
+            flash('ログインに失敗しました')
+            return redirect(url_for('login'))
 
-# 全投稿表示
-@app.route('/top/', methods=["GET"])
-def top():
-    if request.method == "GET":
-        app.logger.info('全投稿表示')
+@app.route('/top', methods=['GET'])
+@login_required
+def toppage():
+    # ログインしたユーザー情報を取得
+    user = User.query.filter_by(name=current_user.name, password=current_user.password).first()
+    app.logger.info('ユーザーネーム:' + user.name)
 
-        contents = Post.query.all()
-        # 投稿保存用の配列
-        posts = []
-        id = 1
-        for content in contents:
-            postDataStr = str(content)[5:]
-            postData = {'id':id, 'content':postDataStr}
-            posts.append(postData)
-            id += 1
-       
-        app.logger.info('ユーザーID:' + str(session['user_id']))
-        return render_template('top.html', user_name=session['name'], posts=posts)
+    return render_template('top.html', user_name=user.name)
 
-# 新規投稿
-@app.route('/post', methods=["GET", "POST"])
-def post():
-    if request.method == "GET":
-        app.logger.info('新規投稿ページ表示')
-        
-        return render_template('post.html')
-    elif request.method == "POST":
-        content = request.form.get('post')
 
-        user = User.query.filter_by(id=session['user_id']).first()
-        app.logger.info(user.id)
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    app.logger.info('ログアウト成功')
 
-        post_data = Post(content=content, user_id=user.id)
-        Post.post(post_data)
-       
-        postContent = Post.query.all()
-        app.logger.info(postContent)
-        
-        # 投稿保存用の配列
-        posts = []
-        id = 1
-        # 配列に投稿を格納
-        for content in postContent:
-            postDataStr = str(content)[5:]
-            postData = {'id':id, 'content':postDataStr}
-            posts.append(postData)
-            id += 1
+    return redirect(url_for('login'))
 
-        app.logger.info('新規投稿完了')
-
-        return render_template('top.html', user_name=session['name'], posts=posts)
-# 投稿削除
-@app.route('/delete', methods=['POST'])
-def delete():
-    from app import db
-
-    id = request.form.get('id')
-    print("削除ID取得:" + str(id))
-    Post.query.filter_by(id=id).delete()
-
-    app.logger.info('削除完了。ID:' + str(id))
-
-    db.session.commit()
-
-    posts = Post.query.all()
-    return render_template('top.html', user_name=session['name'], posts=posts)
 
 # プログラム開始
 def start():
-    # app.run(host='0.0.0.0', port=settings.web_port, threaded=True)
     app.run(debug=True)
